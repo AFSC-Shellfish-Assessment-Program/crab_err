a0dd3bb2a87c2131369da7041ddc3aae2451ce55
Shannon Hennessey <s.hennessey13@gmail.com>
2026-05-12 13:51:37 -0800

    add qaqc queue directory, add more scripts to source

--- Scripts/functions/setup.R
+++ Scripts/functions/setup.R
@@ -1,18 +1,23 @@
 # Setup??
 
-# Load libraries ----
-library(tidyverse)
-library(purrr)
-library(cli) # for colored text
-library(stringi) # read .txt files
-library(data.table) # read .txt files??
-library(rlist) # append to list
+# Load libraries-
+  library(tidyverse)
+  library(purrr)
+  library(cli) # for colored text
+  library(stringi) # read .txt files
+  library(data.table) # read .txt files??
+  library(rlist) # append to list
 
-# Scripts directory....
-scripts_dir <- "./Scripts/"
-source(paste0(scripts_dir, "check_inputs.R"))
-source(paste0(scripts_dir, "file_mgmt_functions.R"))
-source(paste0(scripts_dir, "file_checks_global.R"))
-source(paste0(scripts_dir, "report_setup.R"))
-source(paste0(scripts_dir, "id_zero_catch.R"))
-source(paste0(scripts_dir, "file_checks_haul.R"))
+# Set directory for sourced function scripts
+  scripts_dir <- "./Scripts/functions/"
+  
+# Set directory for QAQC Queue
+  queue_dir <- normalizePath(path = file.path(Sys.getenv("USERPROFILE"), "Desktop/QAQC Queue"), winslash = "/")
+
+# Source scripts with functions for workflow
+  source(paste0(scripts_dir, "check_inputs.R"))
+  source(paste0(scripts_dir, "file_mgmt_functions.R"))
+  source(paste0(scripts_dir, "error_report.R"))
+  source(paste0(scripts_dir, "file_checks_global.R"))
+  source(paste0(scripts_dir, "id_zero_catch.R"))
+  source(paste0(scripts_dir, "file_checks_haul.R"))
