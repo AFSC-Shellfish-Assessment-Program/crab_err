72956b4a6b64654701ee047fd9dfe1600a1e54e6
Shannon Hennessey <s.hennessey13@gmail.com>
2026-05-26 14:20:20 -0800

    suppress load messages for libraries, add message, add haul_checks function

--- Scripts/functions/setup.R
+++ Scripts/functions/setup.R
@@ -1,22 +1,26 @@
-# Setup??
+# Setup
 
 # Start from scratch each time you run
   rm(list = ls())
   
-# Load libraries-
-  library(tidyverse)
-  library(purrr)
-  library(cli) # for colored text and pluralization
-  library(stringi) # read .txt files
-  library(data.table) # read .txt files??
-  library(rlist) # append to list
-
-# Set directory for sourced function scripts
-  scripts_dir <- "./Scripts/functions/"
+# Load libraries
+  libraries <- list("tidyverse",
+                    "purrr",
+                    "cli", # for colored text and pluralization
+                    "stringi", # read .txt files
+                    "data.table", # read .txt files?
+                    "rlist") # append to list
+  suppressMessages(suppressWarnings(invisible(lapply(libraries, library, character.only = TRUE))))
+  
   
 # Set directory for QAQC Queue
   queue_dir <- normalizePath(path = file.path(Sys.getenv("USERPROFILE"), "Desktop/QAQC Queue"), winslash = "/")
 
+  
+# Set directory for sourced function scripts
+  scripts_dir <- "./Scripts/functions/"
+
+  
 # Source scripts with functions for workflow
   source(paste0(scripts_dir, "check_inputs.R"))
   source(paste0(scripts_dir, "file_mgmt_functions.R"))
@@ -26,3 +30,9 @@
   source(paste0(scripts_dir, "file_checks_haul.R"))
   # source(paste0(scripts_dir, "specimen_checks.R"))
   # source(paste0(scripts_dir, "final_haul_checks.R"))
+  # source(paste0(scripts_dir, "haul_checks.R"))
+  
+  
+# Print confirmation message
+  cat("\nLibraries and source functions have been successfully loaded.\n\n")
+  
