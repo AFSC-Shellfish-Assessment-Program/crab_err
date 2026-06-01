6e99e86c8d71fb61d6deec365d62ca7a65d0c9ec
Shannon Hennessey <s.hennessey13@gmail.com>
2026-06-01 12:33:24 -0800

    incorporate 'metadata' list instead of individual function inputs; refine logic and messages; add in report_save and file mgmt functions

--- Scripts/functions/id_zero_catch.R
+++ Scripts/functions/id_zero_catch.R
@@ -1,20 +1,32 @@
 
-# Function to identify zero-catch stations -------------------------------------
+# Function: id_zero_catch ------------------------------------------------------
+#
+# Purpose: Function to identify zero-catch stations
+#
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
 
 #  **do a 2nd iteration of this when just RAW_HAUL is present without a note?? 
 #  and verify 0-catch and say please add a note??
 
 #**NEED TO UPDATE WITH FUNCTION TO SAVE ERROR REPORT AND MOVE FILES TO CLEAN!!!*
 
+
 id_zero_catch <- function(files, 
                           errors,
-                          vessel,
-                          leg,
+                          error_report,
+                          metadata,
                           haul_number,
                           final_haul){
 
+  # Unpack metadata
+    vessel <- metadata$vessel
+    leg <- metadata$leg
+    recorder <- metadata$recorder
+  
+  # Set file directory
+    in_dir <- normalizePath(path = file.path(Sys.getenv("USERPROFILE"), "Desktop/QAQC Queue"), winslash = "/")
+    
   # Set 'haul_id'
-    # haul_id <- str_remove(haul_number, "^0+")
     haul_id <- str_remove(haul_number, "^0+")
       
   # Print message: Was this a 0-catch station? (YES/NO)
@@ -24,25 +36,61 @@ id_zero_catch <- function(files,
   # Select 'YES' this was 0-catch:
     if(no_catch_selection == 1){
       
+      # Add message indicating 0-catch station
+        error_iter <- nrow(errors) + 1
+        errors[error_iter, 1] <- "No Catch"
+        errors[error_iter, 2] <- paste0("Haul ", haul_id, " is a zero-catch station")
+      
+      # Save error report
+        report_save(metadata, 
+                    haul_number = haul_number,
+                    errors = errors,
+                    error_report = error_report)
+      
+      # Move Tablet Files
+        files_clean <- list.files(in_dir, pattern = haul_number, recursive = TRUE)
+        to_clean <- move_files(files = files_clean,
+                               metadata = metadata, 
+                               haul_number = haul_number,
+                               file_type = "tablet",
+                               destination = "clean")
+      
+      # Move Error Report
+        final_report <- move_files(files = list.files(paste0(path, "/Temporary Error Reports/"), 
+                                                      pattern = paste0(vessel, "_", leg, "_Haul", haul_id)),
+                                   metadata = metadata, 
+                                   haul_number = haul_number,
+                                   file_type = "error_report",
+                                   destination = "clean")
+      
+      # Copy files
+        copyFTP <- copy_files(metadata = metadata,  
+                              haul_number = haul_number,
+                              file_type = "tablet", 
+                              destination = "ftp")
+        
+        copyUSB <- copy_files(metadata = metadata,  
+                              haul_number = haul_number,
+                              file_type = "tablet", 
+                              destination = "backup")
+      
+      # Print messages
+        cat("Saving Haul ", haul_id, " final Error Report and moving files to the ", vessel, " ", leg, " 'Clean' folders.\n", sep = "")
+        cat("Copying Haul ", haul_id, " files to the FTP Queue and USB Backup.\n\n", sep = "")
+      
+          
       # If this was the final haul, prompt to review any reports in the Temporary Error Reports folder 
       # and make any corrections etc.
         if(final_haul == TRUE){
-
-          ## OUTPUT ERROR REPORT HERE indicating 0-catch station -- make function for outputting error report and call here!!!!
-          error_iter <- nrow(errors) + 1
-          errors[error_iter, 1] <- "No Catch"
-          errors[error_iter, 2] <- paste0("Haul ", haul_id, " is a zero-catch station")
           
-          ## MOVE FILES
-          ## AND COPY TO sFTP and USB (including error report....)
-          
-          cat("Saving Haul ", haul_id, " final Error Report and moving files to the ", vessel, " ", leg, " 'Clean' folders.\n", sep = "") 
-          cat("This is the final haul in the QAQC Queue. Stopping the error checking protocol.\n\n") 
-          # More instructions here?? Please validate things, or continue with the final lines of the script or something?
-          cat(rep("-", getOption("width")), sep = "")
-          cat("\n\n\n\n\n\n")
-          
-          out <- "break" 
+          # Print messages
+            cat("This is the final haul in the QAQC Queue. Stopping the error checks.\n\n") 
+            cat("Please go back and review any files in the 'Temporary Error Reports' folder on the Desktop and make any data corrections or report annotations before running the final lines of the QAQC script.\n\n")
+            
+            cat(rep("-", getOption("width")), sep = "")
+            cat("\n\n\n\n\n\n")
+            
+            out <- "break" 
         } 
       
       
@@ -53,17 +101,9 @@ id_zero_catch <- function(files,
             
           # Select 'YES' to move to next haul:
             if(move_selection == 1){
+              
+              # Print messages
                 cat("\nYou selected 'Yes'.\n\n", sep = "")
-                
-              ## OUTPUT ERROR REPORT HERE indicating 0-catch station -- make function for outputting error report and call here!!!!
-                error_iter <- nrow(errors) + 1
-                errors[error_iter, 1] <- "No Catch"
-                errors[error_iter, 2] <- paste0("Haul ", haul_id, " is a zero-catch station")
-                
-              ## MOVE FILES
-              ## AND COPY TO sFTP and USB (including error report....)
-                
-                cat("Saving Haul ", haul_id, " final Error Report and moving files to the ", vessel, " ", leg, " 'Clean' folders.\n", sep = "") 
                 cat("Starting error checks for the next haul.\n\n", sep = "")
                 cat(rep("-", getOption("width")), sep = "")
                 cat("\n\n\n\n\n\n")
@@ -74,48 +114,46 @@ id_zero_catch <- function(files,
           
           # Select 'NO' to *NOT* move to next haul:
             if(move_selection == 2){
+              
+              # Print messages
                 cat("\nYou selected 'No'.\n\n", sep = "")
-                
-              ## OUTPUT ERROR REPORT HERE indicating 0-catch station
-                error_iter <- nrow(errors) + 1
-                errors[error_iter, 1] <- "No Catch"
-                errors[error_iter, 2] <- paste0("Haul ", haul_id, " is a zero-catch station")
-                
-              ## MOVE FILES
-              ## AND COPY TO sFTP and USB (including error report...)
-                
-                cat("Saving Haul ", haul_id, " final Error Report and moving files to the ", vessel, " ", leg, " 'Clean' folders.\n", sep = "")
-                cat("Copying Haul ", haul_id, " files to the FTP Queue and USB Backup.\n\n", sep = "")
-                cat(col_red("Stopping the error checking protocol.\n\n", sep = ""))
+                cat(col_red("Stopping the error checks.\n\n", sep = ""))
                 cat(rep("-", getOption("width")), sep = "")
                 cat("\n\n\n\n\n\n")
                 
                 out <- "break"
-            }
-        }
+            } # end not next haul
+        } 
     } 
     
     
   # Select 'NO' there was a catch:
     if(no_catch_selection == 2){
+      
+      # Print messages
         cat("You selected 'No'.\n\n")
         cat(col_red("No 'CATCH' or 'SPECIMEN' files for Haul ", haul_id, " are present in the QAQC Queue.\n", sep = ""))
         cat(col_red("Please make sure those files are in the 'QAQC Queue' folder and try again for this haul.\n\n"))
           
-      #**PRINT ERROR REPORT*
+      # Add error message
         error_iter <- nrow(errors) + 1
         errors[error_iter, 1] <- "File"
         errors[error_iter, 2] <- paste0("No 'CATCH' or 'SPECIMEN' files are present in the QAQC Queue")
       
+      # Save error report
+        report_save(metadata, 
+                    haul_number = haul_number,
+                    errors = errors,
+                    error_report = error_report)
+        
         
       # If this was the final haul, prompt to review any reports in the Temporary Error Reports folder 
       # and make any corrections etc.
         if(final_haul == TRUE){
           
-          # SAVE ERROR REPORT
-          
-            cat("This is the final haul in the QAQC Queue. Saving Haul", haul_id, "temporary Error Report and stopping the error checking protocol.\n\n") 
-            # More instructions here?? Please validate things, or continue with the final lines of the script or something?
+          # Print messages
+            cat("This is the final haul in the QAQC Queue. Stopping the error checks.\n\n") 
+            cat("Please go back and review any files in the 'Temporary Error Reports' folder on the Desktop and make any data corrections or report annotations before running the final lines of the QAQC script.\n\n")
             cat(rep("-", getOption("width")), sep = "")
             cat("\n\n\n\n\n\n")
           
@@ -130,11 +168,9 @@ id_zero_catch <- function(files,
           
           # Select 'YES' move to next haul:
             if(next_selection == 1){
+              
+              # Print messages
                 cat("You selected 'Yes'.\n")
-              
-              # SAVE ERROR REPORT
-              
-              
                 cat("Saving Haul ", haul_id, " temporary Error Report and starting error checks for the next haul.\n\n", sep = "")
                 cat(rep("-", getOption("width")), sep = "")
                 cat("\n\n\n\n\n\n")
@@ -143,13 +179,11 @@ id_zero_catch <- function(files,
             }
           
           # Select 'NO' don't move to next haul:
-            if(next_selection == 2){ #**PRINT ERROR REPORT?*
+            if(next_selection == 2){
+
+              # Print messages 
                 cat(col_red("You selected 'No'.\n"))
-              
-              # SAVE ERROR REPORT
-              
-              
-                cat(col_red("Saving Haul ", haul_id, " temporary Error Report and stopping the error checking protocol.\n\n", sep = ""))
+                cat(col_red("Saving Haul ", haul_id, " temporary Error Report and stopping the error checks.\n\n", sep = ""))
                 cat(rep("-", getOption("width")), sep = "")
                 cat("\n\n\n\n\n\n")
                 
@@ -157,10 +191,6 @@ id_zero_catch <- function(files,
             }
         }  
     }
-    
-    
-    # return(list(errors = errors,
-    #             out = out))  
     
     return(out)
 }
